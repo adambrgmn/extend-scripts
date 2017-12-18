eacd3bb0c5702504a853762d85a2f2e0177d8abc
Adam Bergman <adam@fransvilhelm.com>
2017-12-18 22:47:45 +0100

    feat(babel): Update babelrc and add plugins

--- src/config/babelrc.js
+++ src/config/babelrc.js
@@ -1,24 +1,18 @@
+const isTest = process.env.NODE_ENV === 'test';
+const envModules = !isTest ? { modules: false } : {};
+const envTargets = isTest ? { node: 'current' } : {};
+const envOptions = Object.assign({}, envModules, { targets: envTargets });
+
 module.exports = {
   presets: [
-    [
-      require.resolve('babel-preset-env'),
-      {
-        modules: false,
-      },
-    ],
+    [require.resolve('babel-preset-env'), envOptions],
     require.resolve('babel-preset-flow'),
   ],
   plugins: [
+    require.resolve('babel-macros'),
     require.resolve('babel-plugin-external-helpers'),
     require.resolve('babel-plugin-transform-class-properties'),
     require.resolve('babel-plugin-transform-object-rest-spread'),
-  ],
-  env: {
-    test: {
-      presets: [
-        require.resolve('babel-preset-env'),
-        require.resolve('babel-preset-flow'),
-      ],
-    },
-  },
+    require.resolve('babel-plugin-minify-dead-code-elimination'),
+  ].filter(Boolean),
 };
