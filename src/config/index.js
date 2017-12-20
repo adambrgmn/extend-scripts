aedebf9238045b9b78ab428e6877c454d6ba3943
Adam Bergman <adam@fransvilhelm.com>
2017-12-20 15:37:38 +0100

    fix: Add exports in root of bundle

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  babel: require('./babelrc'),
+  eslint: require('./eslintrc'),
+  jest: require('./jest.config'),
+  lintStaged: require('./lintstagedrc'),
+  prettier: require('./prettierrc'),
+  getRollupConfig: () => require('./rollup.config'),
+  semanticRelease: require('./semantic-release.config'),
+};
